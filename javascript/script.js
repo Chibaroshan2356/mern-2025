91280a3d63995f5b654db7b776d213dbd3d8282f
Chibaroshan <chibaroshan23@gmail.com>
2025-06-19 08:57:38 +0530

    Push all project files

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+var a;
+a=10;
+var a=20;
+let b;
+b=40;
+console.log(b)
+var makrs =[90,91,92,93,94];
+var [m1,m2,m3,m4,m5]=makrs;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+var odd = arr.filter(num => num%2!==0)
+console.log(odd);
+var total = arr.reduce((val,num)=>val+num,0)
+console.log(total)
